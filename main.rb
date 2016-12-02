4f601ba288615546b95ee5389b7f5fe4f714f483
mottox2 <y.mottox2@gmail.com>
2016-12-03 01:58:00 +0900

    Fix

--- main.rb
+++ main.rb
@@ -2,8 +2,9 @@ require_relative './emoji_image'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/json'
+require 'pry'
 
 get '/:text' do
   image = EmojiImage.new params[:text]
-  json ({ path: image.path })
+  json ({ path: request.base_url + image.path })
 end
